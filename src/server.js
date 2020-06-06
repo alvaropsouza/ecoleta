7d88cb01aa035dcec4979a6c91d10c9d574eb3bc
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-06-06 06:00:40 -0300

    Projeto nlw finalizado

--- src/server.js
+++ src/server.js
@@ -62,13 +62,14 @@ server.post("/savepoint", (req, res) => {
 
     function afterInsertData(err) {
         if (err) {
-            return console.log(err)
+            console.log(err)
+            return res.send("Erro no cadastro!")
         }
         
         console.log("Cadastrado com sucesso")
         console.log(this)
 
-        return res.send("ok")
+        return res.render("create-point.html", { saved: true})
     }
 
     db.run(query, values, afterInsertData)
@@ -76,9 +77,16 @@ server.post("/savepoint", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
+    const search = req.query.search
+
+    if(search == "") {
+        //Empty search
+    return res.render("search-results.html", { total: 0})
+    }
+
 
     //grab data from db
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
