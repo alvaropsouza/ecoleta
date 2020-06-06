0575e7159575c7d4fb9131506ace99aa566cc767
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-06-06 04:52:05 -0300

    Update database and db.js

--- src/database/db.js
+++ src/database/db.js
@@ -5,13 +5,15 @@ const sqlite3 = require("sqlite3").verbose()
 // The dot before src means that we are starting the path ate our root folder (nlw)
 const db = new sqlite3.Database("./src/database/database.db")
 
+module.exports = db
+
 // Use database object for our operational
 // db.serialize runs codes in a row with a function
-db.serialize( () => {
+ /* db.serialize( () => {
     // With sql commands i will:
     // 1 create a table(Ex: clients, provinces, citys)
     // Muste use crasis `` in order to 'split' the function 'run'
-    db.run(`
+   db.run(`
         CREATE TABLE IF NOT EXISTS places (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             image TEXT,
@@ -23,7 +25,7 @@ db.serialize( () => {
             items TEXT
         );
     `)
-
+/*
     // 2 Insert data on table
     const query = `
         INSERT INTO places (
@@ -38,13 +40,13 @@ db.serialize( () => {
     `
 
     const values = [
-        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
-        "Colectoria",
-        "Guilherme Gemballa, Jardim América",
-        "Número 260",
-        "Santa Catarina",
-        "Rio do Sul",
-        "Resíduos Eletrônicos, Lâmpadas"
+    "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
+    "Colectoria",
+    "Guilherme Gemballa, Jardim América",
+    "Nº260",
+    "Santa Catarina",
+    "Rio do Sul",
+    "Resíduos Eletrônicos, Lâmpadas"
     ]
 
     function afterInsertData(err) {
@@ -59,7 +61,7 @@ db.serialize( () => {
     db.run(query, values, afterInsertData)
 
      // 3 Search data on table
-     /* db.all(`SELECT name FROM places`, function(err, rows) {
+     db.all(`SELECT name FROM places`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
@@ -67,15 +69,15 @@ db.serialize( () => {
         console.log("Aqui estão seus registros: ")
         console.log(rows);
         
-    })     */
+    })    
 
-    // 4 Delete data on table
-    /* db.run(`DELETE FROM places WHERE id = ?`, [1], function(err) {
+    //4 Delete data on table
+    db.run(`DELETE FROM places WHERE id = ?`, [23], function(err) {
         if(err) {
             return console.log(err)
         }
         console.log("Registro deletado com sucesso!")
         
-    }) */
-})
+    })  
 
+})*/
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")  
 const server = express()
 
+// Grab database
+const db = require("./database/db.js")
+
 // Config 'public' folder, so the server can reach its children folders > assets, styles and scripts
 server.use(express.static("public"))
+
+// Make our express be able to use req.body on our application
+server.use(express.urlencoded({ extended: true }))
 
 // Using Template Engine
 const nunjucks = require("nunjucks")
@@ -19,14 +25,71 @@ server.get("/", (req, res) => {
     return res.render("index.html")
 })
 
+
 server.get("/create-point", (req, res) => {
+
+    // req.query: our url query strings
+    //console.log(req.query);
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+    // req.body: our form body
+    // Insert data into db
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);       
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+        
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.send("ok")
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    //grab data from db
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+        const total = rows.length
+
+    // Show page with db data    
+    return res.render("search-results.html", { places: rows, total: total})
+    })     
+
 })
 
 
 // Turn on server - Listen to port 3000
-server.listen(3000) 
+server.listen(3000)
